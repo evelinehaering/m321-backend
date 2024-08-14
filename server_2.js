b2606dff2848fc94a6576b60425452657f3b9f9c
Eveline <eveline.haering@bbzsogr.ch>
2024-08-14 11:24:21 +0200

    server_2 and server_3

---
+++ server_2.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
+app.use(express.json());
+
+app.post('/increment', async (req, res) => {
+    res.send(`Counter incremented to ${counter}`);
+});
+
+app.get('/counter', (req, res) => {
+    res.json({ counter });
+});
+
+
+app.listen(3000, async () => {
+    console.log('Server running on port 3001')
+});
