8cb6f40b43e7235bdcb9354e690d85e9fdfab811
Eveline <eveline.haering@bbzsogr.ch>
2024-09-11 11:20:13 +0200

    tag test

--- server_2.js
+++ server_2.js
@@ -4,5 +4,4 @@ const port = 3001;
 const serverName = 'server2';
 const nextServerUrl = 'http://localhost:3002/increment';
 const otherServers = ['http://localhost:3000', 'http://localhost:3002'];
-
 createServer(port, serverName, nextServerUrl, otherServers);
