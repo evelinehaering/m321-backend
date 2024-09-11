42585d5d87fa931d535a91682b1135b942662ba8
Eveline <eveline.haering@bbzsogr.ch>
2024-09-11 11:25:09 +0200

    test

--- server_1.js
+++ server_1.js
@@ -6,5 +6,4 @@ const port = 3000;
 const serverName = 'server1';
 const nextServerUrl = 'http://localhost:3001/increment';
 const otherServers = ['http://localhost:3001', 'http://localhost:3002'];
-
 createServer(port, serverName, nextServerUrl, otherServers);
